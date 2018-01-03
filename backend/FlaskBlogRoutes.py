8c13b15eeedcc2d2de467d65b2c9090939cd3a83
William Harding <william.jeffrey.harding@gmail.com>
2018-01-02 18:08:14 -0800

    files added to template, just need to route them and delete the routes in the base flaskapp.py file

--- backend/FlaskBlogRoutes.py
+++ backend/FlaskBlogRoutes.py
@@ -1,3 +1,15 @@
 import yaml
 from flask import Blueprint, render_template,request
 from wtforms import Form, TextField, validators
+
+blog = Blueprint('blog', __name__, template_folder='templates')
+
+blogroutes = [blog]
+
+@blog.route('/blog')
+def blogTemplate():
+	article = request.args.get('c')
+	blogTemplate = bto.getBlogArticle(article)
+	#blogTemplate = yaml.load(open('/home/ubuntu/flaskapp/templates/blogs/example_article_json.json', 'r'))
+	return render_template('blogs/blog_template.html',blogContent=blogTemplate,article=article)
+
